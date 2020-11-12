4581b622d78c87663a436d2cb482e4a4240aedda
Frederico Gomes <frederico.94@hotmail.com>
2020-11-12 15:19:36 +0000

    Product Component

---
+++ foodathome/resources/js/Product/Product.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="jumbotron">
+            <h1>{{ title }}</h1>
+        </div>
+
+        <prod-list :products="products" :columns="columns"></prod-list>
+    </div>
+</template>
+
+<script>
+import ProductListComponent from "./ProductList";
+
+export default {
+    components:{
+        'prod-list': ProductListComponent
+    },
+    data: function () {
+        return {
+            title: 'List of products',
+            products: [],
+            columns: ['id','name','type','description','photo_url']
+        }
+    },
+    methods: {
+        getProducts: function () {
+            axios.get('api/products')
+                .then(response => {
+                    this.products = response.data.data
+                })
+        }
+    },
+    mounted() {
+        this.getProducts();
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

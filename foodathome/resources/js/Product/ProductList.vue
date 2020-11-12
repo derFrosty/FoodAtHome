35d71cc58ed0593abf2a0dd494f28d9d263ccb1a
Frederico Gomes <frederico.94@hotmail.com>
2020-11-12 15:19:27 +0000

    Component that knows how to list products

---
+++ foodathome/resources/js/Product/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-client-table :data="products" :columns="columns" :options="options"/>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['products','columns'],
+    data: function (){
+        return {
+            options: {
+                filterable: ['type']
+            }
+        }
+    }
+
+};
+</script>

d11dfa3e3b3a496979df12273ac14be2ecf5b1ec
Frederico Gomes <frederico.94@hotmail.com>
2020-11-12 11:53:47 +0000

    Simple App.vue with a link for the welcome page

---
+++ foodathome/resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link class="btn btn-primary" to="/">Welcome Page</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>

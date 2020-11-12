f5c1f73e22d29c677c38092f5b5495d47f52ff28
Ivan G. Silva <ivangonzagasilva@live.com.pt>
2020-11-12 16:29:18 +0000

    Route for login and register pages

--- foodathome/resources/js/app.js
+++ foodathome/resources/js/app.js
@@ -6,9 +6,14 @@ import VueRouter from "vue-router";
 import App from './App.vue';
 import WelcomeComponent from "./Welcome/Welcome.vue";
 
+import LoginComponent from "./auth/login.vue";
+import RegisterComponent from "./auth/register.vue";
+
 Vue.use(VueRouter);
 const routes = [
     {path: '/', component: WelcomeComponent},
+    {path: '/login', component: LoginComponent},
+    {path: '/register', component: RegisterComponent},
 ]
 
 const router = new VueRouter({

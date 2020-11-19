b63ed4d966210883e6acc3345197f596cdbbdae3
João Ferreira <2171315@my.ipleiria.pt>
2020-11-19 14:01:34 +0000

    feitinho

--- foodathome/app/Http/Controllers/Api/AuthController.php
+++ foodathome/app/Http/Controllers/Api/AuthController.php
@@ -3,19 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Auth;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
     public function authenticate(Request $request)
     {
-        dd("AQUI0!");
         $credentials = $request->only('email', 'password');
-        dd("AQUI1!");
         if (Auth::attempt($credentials)) {
             // Authentication passed...
-            dd("AQUI2!");
 
             return response()->json(
                 ['msg'=>'Authenticated with success.'],

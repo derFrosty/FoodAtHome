55b5b2498a9c0c36875b89b0a0e26b56489db767
Frederico Gomes <frederico.94@hotmail.com>
2020-12-03 14:12:07 +0000

    Listing all products on the ShoppingCart

--- foodathome/resources/js/User/ShoppingCart.vue
+++ foodathome/resources/js/User/ShoppingCart.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <div v-if="products">
-            <v-client-table :data="products" :columns="columns"></v-client-table>
+        <div v-if="products.length != 0">
+            <v-client-table :data="products" :columns="columns">
+                <template v-slot:photo_url="data">
+                    <img style="display:block;" width="auto" height="65" :src="imgSource(data.row.photo_url)">
+                </template>
+            </v-client-table>
         </div>
         <div v-else>
-            <p>There are no items on you shopping cart</p>
+            <p class="text-xl-center">There are no items on you shopping cart</p>
         </div>
 
     </div>
@@ -12,16 +16,26 @@
 
 <script>
 export default {
-    data: function (){
+    data: function () {
         return {
             products: [],
-            columns: ['photo_url', 'name' , 'quantity', 'options'],
+            columns: ['photo_url', 'name', 'quantity', 'options'],
             headings: {
                 'name': 'Product',
                 'photo_url': 'Photo',
                 'options': ''
             },
 
+        }
+    },
+    methods: {
+        imgSource: function (url) {
+            return "storage/products/" + url;
+        }
+    },
+    mounted() {
+        if (this.$store.state.shoppingCart.length != 0) {
+            this.products = this.$store.state.shoppingCart
         }
     }
 }

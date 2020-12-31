93c8dcc9453391a0d941e4bfed39cdb2f5a89c0f
Frederico Gomes <frederico.94@hotmail.com>
2020-12-31 12:32:15 +0000

    Component that shows a cook his/hers dashboard

---
+++ foodathome/resources/js/Dashboard/CookDashboard.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>{{ title }}</h1>
+        </div>
+
+        <div>
+            <p class="text-xl-center">There are no items on you shopping cart</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function (){
+        return {
+            title: "Dashboard"
+        }
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

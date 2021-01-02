adf01cf3ae99d84b5e13a9095cd181d9e5ba9d73
Frederico Gomes <frederico.94@hotmail.com>
2021-01-02 15:49:29 +0000

    get order and order_items

--- foodathome/app/Http/Controllers/Api/CookApiController.php
+++ foodathome/app/Http/Controllers/Api/CookApiController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\Order_Item;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,8 +12,21 @@ class CookApiController extends Controller
 {
     public function index(Request $request)
     {
-        if (Auth::user()->type != 'EC'){
+        $user = Auth::user();
+
+        if (!$user || $user['type'] != 'EC'){
             abort(403);
+        }
+
+        //null == cook is preparing something
+        if (!$user['available_at']){
+
+            $order = Order::Where('prepared_by', $user['id'])->Where('status', 'P')->first();
+
+            $order_items = Order_Item::Where('order_id',$order['id'])->get();
+
+            dd($order_items);
+
         }
 
         return 'boas';

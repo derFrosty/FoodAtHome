7cc193fc4d3922028f0ea7b35ca765c52faad105
Frederico Gomes <frederico.94@hotmail.com>
2021-01-03 18:34:02 +0000

    show all users

--- foodathome/resources/js/User/UserList.vue
+++ foodathome/resources/js/User/UserList.vue
@@ -3,15 +3,56 @@
         <div class="jumbotron">
             <h1>{{ title }}</h1>
         </div>
+        <v-client-table :data="users" :columns="columns" :options="options">
+            <template v-slot:photo_url="data">
+                <img style="display:block;" width="auto" height="65" :src="imgSource(data.row.photo_url)">
+            </template>
+            <template v-slot:blocked="data">
+                <p>{{isBlocked(data.row.blocked)}}</p>
+            </template>
+        </v-client-table>
     </div>
 </template>
 
 <script>
 export default {
-    data: function (){
+    data: function () {
         return {
-            title: 'User List'
+            title: 'User List',
+            users: [],
+            columns: ['photo_url' ,'id', 'name', 'email', 'blocked', 'options'],
+            options: {
+                filterable: ['name', 'type'],
+                headings: {
+                    'photo_url': 'Photo',
+                    'options': '',
+                    'email': 'E-mail'
+                },
+                sortable: ['id','name','email','blocked']
+            }
         }
+    },
+    methods: {
+        getAllUsers: function () {
+            axios.get('api/users')
+                .then(response => {
+                    this.users = response.data.data;
+                })
+        },
+        isBlocked: function (status){
+            switch (status){
+                case 1:
+                    return 'Blocked';
+                case 0:
+                    return 'Active';
+            }
+        },
+        imgSource: function (url) {
+            return url != null ? "storage/fotos/" + url : "storage/fotos/template.png";
+        }
+    },
+    mounted() {
+        this.getAllUsers()
     }
 }
 </script>

d779c0b6b7f03bbab5d8cf3db9f305c43a7e001f
Frederico Gomes <frederico.94@hotmail.com>
2021-01-03 18:04:13 +0000

    component that lists all users (incomplete)

---
+++ foodathome/resources/js/User/UserList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>{{ title }}</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: function (){
+        return {
+            title: 'User List'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>

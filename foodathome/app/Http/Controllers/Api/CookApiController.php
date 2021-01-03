52b4edc74dc226dfe5e0682697d15d709351dd0c
Frederico Gomes <frederico.94@hotmail.com>
2021-01-03 09:52:39 +0000

    get order that cook is preparing

--- foodathome/app/Http/Controllers/Api/CookApiController.php
+++ foodathome/app/Http/Controllers/Api/CookApiController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use App\Models\Order_Item;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,10 +27,38 @@ class CookApiController extends Controller
 
             $order_items = Order_Item::Where('order_id',$order['id'])->get();
 
-            dd($order_items);
+            $customer = User::find($order['customer_id']);
+
+            $preparing_order = [];
+            $preparing_order['customer']['id'] = $customer['id'];
+            $preparing_order['customer']['name'] = $customer['name'];
+
+            $preparing_order['order'] = $order;
+            $preparing_order['order_items'] = $order_items;
+
+            foreach ($preparing_order['order_items'] as $key => $item){
+                $product = Product::find($item['product_id']);
+
+                $preparing_order['order_items'][$key]['name'] = $product['name'];
+                $preparing_order['order_items'][$key]['photo_url'] = $product['photo_url'];
+            }
+
+            return response()->json(
+                [
+                    'msg' => 'true',
+                    'preparing_order' => $preparing_order
+                ],
+                200
+            );
 
         }
 
-        return 'boas';
+        return response()->json(
+            [
+                'msg' => 'false',
+                'preparing_order' => []
+            ],
+            200
+        );
     }
 }
